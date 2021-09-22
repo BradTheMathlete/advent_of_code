322997c79a58502133057f379e3155040068c38a
Brad Hardy <bhardy1@jaguarlandrover.com>
2021-09-22 08:31:03 +0100

    re-organise folder structure

--- 2020/solutions/day_01.py
+++ 2020/solutions/day_01.py
@@ -1,5 +1,5 @@
 test_input = [1721, 979, 366, 299, 675, 1456]
-challenge_input_loc = "2020/day_01/input.txt"
+challenge_input_loc = "2020/inputs/day_01.txt"
 challenge_input = open(challenge_input_loc).read().splitlines()
 challenge_input = [eval(i) for i in challenge_input]
 
@@ -16,8 +16,9 @@ print(challenge_01(challenge_input))
 
 
 def challenge_02(entries: list) -> int:
-    for index_i, i in enumerate(entries[:len(entries) - 2]):
-        for index_j, j in enumerate(entries[index_i + 1:len(entries) - 1]):
+    no_entries = len(entries)
+    for index_i, i in enumerate(entries[:no_entries - 2]):
+        for index_j, j in enumerate(entries[index_i + 1:no_entries - 1]):
             for k in entries[index_j + 1:]:
                 if i + j + k == 2020:
                     return i * j * k
