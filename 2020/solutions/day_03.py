0d05e077ac88fb475c0dd8eb64ce595beab902d9
Brad Hardy <bhardy1@jaguarlandrover.com>
2021-09-24 14:25:55 +0100

    solve part 2 of 2020's day 3 challenge

--- 2020/solutions/day_03.py
+++ 2020/solutions/day_03.py
@@ -29,3 +29,22 @@ def challenge_01(geology: list, right: int, down: int) -> int:
 
 print(challenge_01(test_input, 3, 1))
 print(challenge_01(challenge_input, 3, 1))
+
+slopes = [
+    {"right": 1, "down": 1},
+    {"right": 3, "down": 1},
+    {"right": 5, "down": 1},
+    {"right": 7, "down": 1},
+    {"right": 1, "down": 2},
+]
+
+
+def challenge_02(geology: list, traversals: list) -> int:
+    total_trees = 1
+    for i in traversals:
+        total_trees *= challenge_01(geology, i['right'], i['down'])
+    return total_trees
+
+
+print(challenge_02(test_input, slopes))
+print(challenge_02(challenge_input, slopes))
