63f70d133f00893acb5de48a4663a169716b35f0
Brad Hardy <bhardy1@jaguarlandrover.com>
2022-12-16 11:37:12 +0000

    solve part 2 of 2022's day 2 challenge

--- 2022/solutions/day_02.py
+++ 2022/solutions/day_02.py
@@ -7,7 +7,7 @@ def process_input(input_data):
     return [i.split(' ') for i in input_data.split('\n')]
 
 
-def score_strategy(processed_data):
+def score_strategy_1(processed_data):
     score = 0
     for r in processed_data:
         if r[1] == 'X':
@@ -20,4 +20,27 @@ def score_strategy(processed_data):
             score += 3
         elif r in [['A', 'Y'], ['B', 'Z'], ['C', 'X']]:
             score += 6
+    return score
+
+
+def score_strategy_2(processed_data):
+    win_dict = {'A': 'P', 'B': 'S', 'C': 'R'}
+    draw_dict = {'A': 'R', 'B': 'P', 'C': 'S'}
+    lose_dict = {'A': 'S', 'B': 'R', 'C': 'P'}
+    score = 0
+    for r in processed_data:
+        if r[1] == 'X':
+            shape = lose_dict[r[0]]
+        elif r[1] == 'Y':
+            score += 3
+            shape = draw_dict[r[0]]
+        else:
+            score += 6
+            shape = win_dict[r[0]]
+        if shape == 'R':
+            score += 1
+        elif shape == 'P':
+            score += 2
+        else:
+            score += 3
     return score
