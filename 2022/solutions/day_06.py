8dc8dcd8d0981a1e6893530fb14155583631229f
Brad Hardy <bhardy1@jaguarlandrover.com>
2022-12-20 12:53:12 +0000

    solve part 2 of 2022's day 6 challenge

--- 2022/solutions/day_06.py
+++ 2022/solutions/day_06.py
@@ -7,9 +7,13 @@ challenge_input_loc = "2022/inputs/day_06.txt"
 challenge_input = open(challenge_input_loc).read()
 
 
-def find_subroutine(stream):
-    loc = 4
+def find_subroutine(stream, dist_len):
+    loc = dist_len
     while True:
-        if len(set((stream[loc - 4:loc]))) == 4:
+        if len(set((stream[loc - dist_len:loc]))) == dist_len:
             return loc
         loc += 1
+
+
+p1_output = find_subroutine(challenge_input, 4)
+p2_output = find_subroutine(challenge_input, 14)
