60db058eed161ccd3305a9ddebf0a7da8a446e5c
Hardy <BHARDY1@jaguarlandrover.com>
2024-12-04 11:02:59 +0000

    reinstate files

---
+++ 2024/solutions/day_02.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    test_input = '3   4\n4   3\n2   5\n1   3\n3   9\n3   3'
+    # challenge_input = open("2024/input.txt").read()
+
+    # test_01, test_02 = main(test_input)
+    # challenge_01, challenge_02 = main(challenge_input)
+
+    print('finished')
