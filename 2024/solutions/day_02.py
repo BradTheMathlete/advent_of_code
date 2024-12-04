1d6e04f8308220e17187338c9439802bb89d02db
Hardy <BHARDY1@jaguarlandrover.com>
2024-12-04 11:47:26 +0000

    solve part 2 of 2024's day 2 challenge

--- 2024/solutions/day_02.py
+++ 2024/solutions/day_02.py
@@ -14,22 +14,35 @@ def level_is_safe(level_data) -> bool:
             return False
     return True
 
-def task_01(level_data) -> int:
-    safe_levels = [level_is_safe(l) for l in level_data]
+def task_01(report_data) -> int:
+    safe_levels = [level_is_safe(l) for l in report_data]
     return sum(safe_levels)
+
+def level_is_safe_with_dampening(level_data) -> bool:
+    for i in range(len(level_data)):
+        temp = [j for index, j in enumerate(level_data) if index != i]
+        if level_is_safe(temp):
+            return True
+    return False
+
+
+def task_02(report_data) -> int:
+    safe_levels_with_dampening = [level_is_safe_with_dampening(l) for l in report_data]
+    return sum(safe_levels_with_dampening)
 
 def main(input_data):
     processed_data = process_input(input_data)
     output_01 = task_01(processed_data)
-    return output_01
+    output_02 = task_02(processed_data)
+    return output_01, output_02
 
 if __name__ == "__main__":
     test_input = '7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9'
     challenge_input = open("2024/inputs/day_02.txt").read()
 
-    test_01 = main(test_input)
-    # test_01, test_02 = main(test_input)
-    challenge_01 = main(challenge_input)
-    # challenge_01, challenge_02 = main(challenge_input)
+    # test_01 = main(test_input)
+    test_01, test_02 = main(test_input)
+    # challenge_01 = main(challenge_input)
+    challenge_01, challenge_02 = main(challenge_input)
 
     print('finished')
