6eeb258bc21b1b7a2ed8deb3304e8453103f4025
Hardy <BHARDY1@jaguarlandrover.com>
2024-12-05 09:56:28 +0000

    solve part 2 of 2024's day 4 challenge

--- 2024/solutions/day_04.py
+++ 2024/solutions/day_04.py
@@ -30,18 +30,39 @@ def task_01(puzzle_input) -> int:
         count += search_for_xmas(np.flip(d_b).tolist())
     return count
 
+def check_for_x_mas(box_to_search) -> bool:
+    target = list('MAS')
+    d_f = np.diag(box_to_search)
+    if (not d_f.tolist() == target) and (not np.flip(d_f).tolist() == target):
+        return False
+    d_b = np.diag(np.flip(box_to_search, axis=1))
+    if (not d_b.tolist() == target) and (not np.flip(d_b).tolist() == target):
+        return False
+    return True
+
+def task_02(puzzle_input) -> int:
+    count = 0
+    puzzle_size = puzzle_input.shape[0]
+    for i in range(puzzle_size + 1 - 3):
+        for j in range(puzzle_size + 1 - 3):
+            box = np.array([row[j:j+3] for row in puzzle_input[i:i+3]])
+            if check_for_x_mas(box):
+                count += 1
+    return count
+
 def main(input_data):
     processed_data = process_input(input_data)
     output_01 = task_01(processed_data)
-    return output_01
+    output_02 = task_02(processed_data)
+    return output_01, output_02
 
 if __name__ == "__main__":
     test_input = 'MMMSXXMASM\nMSAMXMSMSA\nAMXSXMAAMM\nMSAMASMSMX\nXMASAMXAMM\nXXAMMXXAMA\nSMSMSASXSS\nSAXAMASAAA\nMAMMMXMMMM\nMXMXAXMASX'
     challenge_input = open("2024/inputs/day_04.txt").read()
 
-    test_01 = main(test_input)
-    # test_01, test_02 = main(test_input)
-    challenge_01 = main(challenge_input)
-    # challenge_01, challenge_02 = main(challenge_input)
+    # test_01 = main(test_input)
+    test_01, test_02 = main(test_input)
+    # challenge_01 = main(challenge_input)
+    challenge_01, challenge_02 = main(challenge_input)
 
     print('finished')
