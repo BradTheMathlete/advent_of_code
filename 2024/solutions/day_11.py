5f68909f68524e0276c0d821de851ffad6a5b330
Hardy <BHARDY1@jaguarlandrover.com>
2024-12-17 11:49:38 +0000

    solve part 2 of 2024's day 11 challenge

--- 2024/solutions/day_11.py
+++ 2024/solutions/day_11.py
@@ -27,19 +27,47 @@ def task_01(initial_stones, no_blinks) -> int:
         final_stones = update_stones(final_stones)
     return len(final_stones)
 
+def convert_to_dict(input_stones):
+    stones_dict = {}
+    for value in set(input_stones):
+        stones_dict[value] = input_stones.count(value)
+    return stones_dict
+
+def update_stones_dict(input_stones_dict):
+    u_stones_dict = {}
+    for value, count in input_stones_dict.items():
+        if value == 0:
+            new_values = [1]
+        elif len(str(value)) % 2 == 0:
+            new_values = split_stone(value)
+        else:
+            new_values = [value * 2024]
+        for n in new_values:
+            if n in u_stones_dict:
+                u_stones_dict[n] += count
+            else:
+                u_stones_dict[n] = count
+    return u_stones_dict
+
+def task_02(initial_stones, no_blinks) -> int:
+    final_stones_dict = convert_to_dict(initial_stones)
+    for _ in range(no_blinks):
+        final_stones_dict = update_stones_dict(final_stones_dict)
+    return sum(final_stones_dict.values())
+
 def main(input_data):
     initial_stones = process_input(input_data)
     output_01 = task_01(initial_stones, no_blinks=25)
-    # output_02 = task_02(initial_stones)
-    return output_01
+    output_02 = task_02(initial_stones, no_blinks=75)
+    return output_01, output_02
 
 if __name__ == "__main__":
     test_input = '125 17'
     challenge_input = open("2024/inputs/day_11.txt").read()
 
-    test_01 = main(test_input)
-    # test_01, test_02 = main(test_input)
-    challenge_01 = main(challenge_input)
-    # challenge_01, challenge_02 = main(challenge_input)
+    # test_01 = main(test_input)
+    test_01, test_02 = main(test_input)
+    # challenge_01 = main(challenge_input)
+    challenge_01, challenge_02 = main(challenge_input)
 
     print('finished')
